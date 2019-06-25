ba922e1e8474606f4b2e485fb5426c0bcb311e70
DanniUCI <dannyxiong2016@gmail.com>
2019-06-24 21:31:55 -0700

    practice for June/25

---
+++ LC0019.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        fast = head
+        dummy = ListNode(0)
+        dummy.next = head
+        slow = dummy
+        while n > 0 and fast:
+            fast = fast.next
+            n -= 1
+        while fast:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next
